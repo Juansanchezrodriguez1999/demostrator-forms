e1adde416022711c6eabd6acf4145c891fcb977d
juansanchezrodriguez1999 <juansanchezrodriguez1999@gmail.com>
2025-05-06 10:57:01 +0200

    delform1

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ export default function Home() {
     <div className="px-6 pt-4 pb-2">
       <div><Link className="inline-block bg-[#8D8F93] text-white px-4 py-2 rounded text-2xl font-semibold text-gray-700 mr-2 mb-2 hover:bg-[#75767a]" href={"auth/login"}>Login</Link></div>
       <div><Link className="inline-block bg-[#8D8F93] text-white px-4 py-2 rounded text-2xl font-semibold text-gray-700 mr-2 mb-2 hover:bg-[#75767a]" href={"auth/register"}>Register</Link></div>
-      
-      <div><Link className="inline-block bg-[#8D8F93] text-white px-4 py-2 rounded text-2xl font-semibold text-gray-700 mr-2 mb-2 hover:bg-[#75767a]" href={"forms/form1"}>MI_F01</Link></div>
       <div><Link className="inline-block bg-[#8D8F93] text-white px-4 py-2 rounded text-2xl font-semibold text-gray-700 mr-2 mb-2 hover:bg-[#75767a]" href={"forms/form2"}>CE_F01</Link></div>
       <div><Link className="inline-block bg-[#8D8F93] text-white px-4 py-2 rounded text-2xl font-semibold text-gray-700 mr-2 mb-2 hover:bg-[#75767a]" href={"forms/form3"}>EN_F01</Link></div>
       {session && (
